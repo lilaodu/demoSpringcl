be2f21abe94123a8de89e59516d4e7ca51484cb2
wokanjianve@163.com <s6477113>
2019-11-20 11:38:06 +0800

    1

---
+++ webapi-8001/src/main/java/com/g/entity/Order.java
@@ -0,0 +1,16 @@
+package com.g.entity;
+
+public class Order {
+
+	
+	private String type;//
+	
+	private Integer total;//金额 （100倍数）
+	
+	private String apiOrderSn;//订单编号
+	
+	private String client_id;//商户id
+	
+	
+	
+}
