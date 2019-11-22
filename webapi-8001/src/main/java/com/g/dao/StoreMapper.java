000179162f089e0d0b7cc6dade722d92d94ff18e
lilaodu <ljk_awe@163.com>
2019-11-22 17:44:17 +0800

    dgg

---
+++ webapi-8001/src/main/java/com/g/dao/StoreMapper.java
@@ -0,0 +1,31 @@
+package com.g.dao;
+
+import java.util.List;
+
+import com.entity.Store;
+import com.entity.StoreExample;
+import org.apache.ibatis.annotations.Param;
+
+public interface StoreMapper {
+    int countByExample(StoreExample example);
+
+    int deleteByExample(StoreExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Store record);
+
+    int insertSelective(Store record);
+
+    List<Store> selectByExample(StoreExample example);
+
+    Store selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Store record, @Param("example") StoreExample example);
+
+    int updateByExample(@Param("record") Store record, @Param("example") StoreExample example);
+
+    int updateByPrimaryKeySelective(Store record);
+
+    int updateByPrimaryKey(Store record);
+}
